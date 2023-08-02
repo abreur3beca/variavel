189176aa8cdee0a5dc3b590ff98f3a1c034e1ef4
abreur3beca <123771701+abreur3beca@users.noreply.github.com>
2023-08-02 10:54:16 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,75 @@
+//atividade 1
+var favoritefood = "pizza";
+
+//atividade 2
+var numOfSlices = 8;
+
+//atividade 3
+console.log(favoritefood);
+
+//atividade 4
+console.log (numOfSlices);
+
+/*atividade 5
+As declarações de var tem escopo global ou de função, enquanto as declarações de let e de const têm escopo de bloco.
+Variáveis de var podem ser modificadas e expostas novamente dentro de seu escopo. 
+As variáveis de let podem ser atualizadas, mas não podem ser declaradas novamente. 
+As variáveis de const não podem ser modificadas.
+*/
+
+//atividade 6 e 7
+let changeMe = true;
+changeMe = false;
+
+//atividade 8
+console.log(changeMe);
+
+//atividade 9
+const entree = "enchiladas";
+
+//atividade 10 
+console.log(entree);
+
+//atividade 11
+//entree = "cocada"
+
+//atividade 12
+let levelUp = 15;
+levelUp += 5;
+console.log(levelUp);
+
+//atividade 13
+let powerLevel = 200;
+powerLevel -= 100;
+console.log(powerLevel);
+
+//atividade 14
+let multiplyMe = 18;
+multiplyMe *= 11;
+console.log(multiplyMe);
+
+//atividade 15
+let quarterMe = 24;
+quarterMe /= 4;
+console.log(quarterMe);
+
+//atividade 16
+let gainedDollar = 12;
+gainedDollar ++;
+console.log(gainedDollar);
+
+//atividade 17
+let lostDollar = 17;
+lostDollar --;
+console.log(lostDollar);
+
+//atividade 18
+let NAME = "Rebeca";
+let CITY = "Osasco";
+console.log( `My name is ${NAME}. My favorite city is ${CITY}` );
+
+//atividade 19
+let newVarible = 18;
+console.log(typeof newVarible);
+
+
